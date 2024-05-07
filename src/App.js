b88ff2d358c5b9b1960c6b61eed7b61439a01802
jacob Plumb <jacobplumb@jacobs-MBP.attlocal.net>
2024-05-07 17:45:36 -0400

    added schema

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import { useState } from "react";
+import * as yup from "yup";
 
 const intialFormValues = {
-  fullName: '',
-  description: ''
-}
+  fullName: "",
+  description: "",
+};
+
+const schema = yup.object().shape({
+  fullName: yup
+    .string()
+    .required("this field is required")
+    .min(3, "full name must be at least 3 characters")
+    .max(20, "full name must be at most 20 charaters"),
+});
 
 function App() {
   const [formValues, setFormValues] = useState(intialFormValues);
 
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      console.log(valid);
+    })
+  }, [formValues])
+
   const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setFormValues({...formValues, [name]: value}) // {description: '', fullName: 'a'}
-  }
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value }); // {description: '', fullName: 'a'}
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formValues);
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input onChange={handleInputChange} name="fullName" value={formValues.fullName} />
-      <input onChange={handleInputChange} name="description" value={formValues.description} />
+      <input
+        onChange={handleInputChange}
+        name="fullName"
+        value={formValues.fullName}
+      />
+      <input
+        onChange={handleInputChange}
+        name="description"
+        value={formValues.description}
+      />
       <button>Submit</button>
     </form>
   );
